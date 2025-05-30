3a340ddad9e0e8958a8e717aac76ae60c116763f
priyanshukumar72 <priyanshu727989@gmail.com>
2025-05-30 13:37:22 +0530

    delete and modify

--- submitresult.js
+++ submitresult.js
@@ -1,15 +1,10 @@
 const results = [
-  {
-    roll_no: "101",
-    Date: "03/05/2025",//mm/dd/yy
-    Math: "30",
-    English: "35",
-    Hindi: "40"
-  }
-
-
-
-
-
+//   {
+//     roll_no: "101",
+//     Date: "03/05/2025",//mm/dd/yy
+//     Math: "30",
+//     English: "35",
+//     Hindi: "40"
+//   }
 ];
 module.exports = results;
