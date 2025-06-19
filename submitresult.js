a124a5c2a620be09b07b604e9fe73b31ef144e50
Sachin kumar <130838646+Sachin8757@users.noreply.github.com>
2025-06-19 23:21:54 +0530

    Update submitresult.js

--- submitresult.js
+++ submitresult.js
@@ -1,11 +1,54 @@
 const results = [
-//   {
-//     roll_no: "101",
-//     Date: "03/05/2025",//mm/dd/yy
-//     Math: "41",
-//     English: "0",
-//     Hindi: "19"
-//   },
-
+  {
+    roll_no: "101",
+    Date: "06/16/2025",
+    Math: "30",
+    Hindi: "50",
+    English: "3"
+  },
+  {
+    roll_no: "106",
+    Date: "06/16/2025",
+    Math: "0",
+    Hindi: "0",
+    English: "0"
+  },
+  {
+    roll_no: "108",
+    Date: "06/16/2025",
+    Math: "15",
+    Hindi: "50",
+    English: "25"
+  },
+  {
+    roll_no: "109",
+    Date: "06/16/2025",
+    Math: "50",
+    Hindi: "45",
+    English: "45"
+  },
+  {
+    roll_no: "110",
+    Date: "06/16/2025",
+    Math: "35",
+    Hindi: "50",
+    English: "40"
+  },
+  {
+    roll_no: "111",
+    Date: "06/16/2025",
+    Math: "50",
+    Hindi: "50",
+    English: "42"
+  },
+  {
+    roll_no: "107",
+    Date: "06/16/2025",
+    Math: "0",
+    Hindi: "0",
+    English: "0"
+  }
 ];
+
 module.exports = results;
+
