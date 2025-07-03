8d0ceadf703a20896a5c3df394aea278e911668b
priyanshukumar72 <priyanshu727989@gmail.com>
2025-07-03 23:31:20 +0530

    result out

--- submitresult.js
+++ submitresult.js
@@ -1,53 +1,68 @@
 const results = [
   {
+    roll_no: "107",
+    Math: 29,
+    English: 31,
+    Hindi: 35,
+    Behaviour: 25
+  },
+  {
     roll_no: "101",
-    Math: 30,
-    English: 3,
-    Hindi: 50,
+    Math: 34,
+    English: 39,
+    Hindi: 45,
+    Behaviour: 45
+  },
+  {
+    roll_no: "102",
+    Math: 28,
+    English: 35,
+    Hindi: 40,
+    Behaviour: 45
+  },
+  {
+    roll_no: "109",
+    Math: 25,
+    English: 37,
+    Hindi: 25,
     Behaviour: 40
   },
   {
-    roll_no: "106",
-    Math: 0,
-    English: 0,
-    Hindi: 0,
+    roll_no: "115",
+    Math: 40,
+    English: 25,
+    Hindi: 23,
+    Behaviour: 30
+  },
+  {
+    roll_no: "118",
+    Math: 15,
+    English: 24,
+    Hindi: 20,
+    Behaviour: 45
+  },
+  {
+    roll_no: "117",
+    Math: 5,
+    English: 30,
+    Hindi: 25,
     Behaviour: 40
   },
   {
     roll_no: "108",
-    Math: 15,
-    English: 25,
-    Hindi: 50,
-    Behaviour: 40
-  },
-  {
-    roll_no: "109",
-    Math: 50,
-    English: 45,
-    Hindi: 45,
+    Math: 10,
+    English: 40,
+    Hindi: 35,
     Behaviour: 40
   },
   {
     roll_no: "110",
-    Math: 35,
-    English: 40,
-    Hindi: 50,
-    Behaviour: 40
-  },
-  {
-    roll_no: "111",
-    Math: 50,
-    English: 42,
-    Hindi: 50,
-    Behaviour: 40
-  },
-  {
-    roll_no: "107",
-    Math: 0,
-    English: 0,
-    Hindi: 0,
-    Behaviour: 40
+    Math: 10,
+    English: 24,
+    Hindi: 30,
+    Behaviour: 35
   }
 ];
 
 module.exports = results;
+
