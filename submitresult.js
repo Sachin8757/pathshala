05e7e001a9ef87790a996004380ecba20003f676
priyanshukumar72 <priyanshu727989@gmail.com>
2025-08-04 22:04:48 +0530

    result of 01/08/025

--- submitresult.js
+++ submitresult.js
@@ -1,68 +1,34 @@
 const results = [
   {
-    roll_no: "107",
-    Math: 29,
-    English: 31,
-    Hindi: 35,
-    Behaviour: 25
-  },
-  {
-    roll_no: "101",
-    Math: 34,
-    English: 39,
-    Hindi: 45,
-    Behaviour: 45
-  },
-  {
-    roll_no: "102",
-    Math: 28,
-    English: 35,
-    Hindi: 40,
-    Behaviour: 45
+    roll_no: "108",
+    Math: 45,
+    Hindi: 43,
+    English: 44,
   },
   {
     roll_no: "109",
-    Math: 25,
-    English: 37,
-    Hindi: 25,
-    Behaviour: 40
-  },
-  {
-    roll_no: "115",
-    Math: 40,
-    English: 25,
-    Hindi: 23,
-    Behaviour: 30
-  },
-  {
-    roll_no: "118",
-    Math: 15,
-    English: 24,
-    Hindi: 20,
-    Behaviour: 45
+    Math: 45,
+    Hindi: 33,
+    English: 44,
   },
   {
     roll_no: "117",
-    Math: 5,
-    English: 30,
-    Hindi: 25,
-    Behaviour: 40
-  },
-  {
-    roll_no: "108",
-    Math: 10,
-    English: 40,
-    Hindi: 35,
-    Behaviour: 40
+    Math: 45,
+    Hindi: 29,
+    English: 37,
   },
   {
     roll_no: "110",
-    Math: 10,
-    English: 24,
-    Hindi: 30,
-    Behaviour: 35
+    Math: 40,
+    Hindi: 35,
+    English: 43,
+  },
+  {
+    roll_no: "115",
+    Math: 42,
+    Hindi: 32,
+    English: 15,
   }
 ];
 
 module.exports = results;
-
