3c2e869ba9c7a5d8d112a76a8965df0654d14abc
priyanshukumar72 <priyanshu727989@gmail.com>
2025-08-05 08:13:41 +0530

    result of 01/08/025

--- submitresult.js
+++ submitresult.js
@@ -4,30 +4,39 @@ const results = [
     Math: 45,
     Hindi: 43,
     English: 44,
+    Behaviour: 45
   },
   {
     roll_no: "109",
     Math: 45,
     Hindi: 33,
     English: 44,
+    Behaviour: 40
+
   },
   {
     roll_no: "117",
     Math: 45,
     Hindi: 29,
     English: 37,
+    Behaviour: 30
+
   },
   {
     roll_no: "110",
     Math: 40,
     Hindi: 35,
     English: 43,
+    Behaviour: 35
+
   },
   {
     roll_no: "115",
     Math: 42,
     Hindi: 32,
     English: 15,
+    Behaviour: 40
+
   }
 ];
 
